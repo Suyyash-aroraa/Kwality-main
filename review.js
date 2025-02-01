ac64dddd85ce81702e382cddf8dc691408ee09f1
Suyyash_Ar <114338825+Suyyash-aroraa@users.noreply.github.com>
2025-02-01 22:26:09 +0530

    Update review.js

--- review.js
+++ review.js
@@ -1,11 +1,3 @@
-document.getElementById("close-great").addEventListener("click", () => {
-  document.getElementById("great-options").style.visibility = "hidden";
-  console.log("work");
-});
-
-document.getElementById("great-btn").addEventListener("click", () => {
-  document.getElementById("great-options").style.visibility = "visible";
-});
 
 document.getElementById("close-not-great").addEventListener("click", () => {
   document.getElementById("not-great-options").style.visibility = "hidden";
